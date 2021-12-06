6c4f6f942db18dee2fe40cedad393d8989b73181
jerskisnow <jerskisnow@gmail.com>
2021-12-06 23:54:51 +0100

    Versions update + Added card support

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,20 @@ import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
 
-import "./App.css";
+import './App.css'
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+// https://material.io/design/color/dark-theme.html#ui-application
+// https://react-bootstrap.github.io/getting-started/introduction/
+// https://unsplash.com/
+
+const homeCampaigns = [
+    { image: 'https://images.unsplash.com/photo-1605792657660-596af9009e82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1102&q=80', title: 'My new project', adr: '0xaACC88D8C3D9bFdd781dEa1381c073E205796970', desc: 'Hi all! I would like to start my new project the only problem I ran into is that i need some funding to kickstart it. That\'s why I created this funding campaign on this amazing platform.' },
+    { image: 'https://images.unsplash.com/photo-1590283603385-17ffb3a7f29f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y3J5cHRvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', title: 'Lorem ipsum', adr: '0xaACC88D8C3D9bFdd781dEa1381c073E205796970', desc: 'dolor sit amet.' },
+    { image: 'https://images.unsplash.com/photo-1622630998477-20aa696ecb05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNyeXB0b3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', desc: 'Hi all! I would like to start my new project the only problem I ran into is that i need some funding to kickstart it. That\'s why I created this funding campaign on this amazing platformawddddddddddddddddddddddddddddd.' }
+]
 
 class App extends Component {
     state = { storageValue: 0, web3: null, accounts: null, contract: null };
@@ -23,27 +36,16 @@ class App extends Component {
                 deployedNetwork && deployedNetwork.address,
             );
 
+            console.log(accounts)
+
             // Set web3, accounts, and contract to the state, and then proceed with an
             // example of interacting with the contract's methods.
-            this.setState({ web3, accounts, contract: instance }, this.runExample);
+            this.setState({ web3, accounts, contract: instance });
         } catch (error) {
             // Catch any errors for any of the above operations.
             alert(`Failed to load web3, accounts, or contract. Check console for details.`);
             console.error(error);
         }
-    };
-
-    runExample = async () => {
-        const { accounts, contract } = this.state;
-
-        // Stores a given value, 5 by default.
-        await contract.methods.set(5).send({ from: accounts[0] });
-
-        // Get the value from the contract to prove it worked.
-        const response = await contract.methods.get().call();
-
-        // Update state with the result.
-        this.setState({ storageValue: response });
     };
 
     render() {
@@ -52,17 +54,25 @@ class App extends Component {
         }
         return (
             <div className="App">
-                <h1>Good to Go!</h1>
-                <p>Your Truffle Box is installed and ready.</p>
-                <h2>Smart Contract Example</h2>
-                <p>
-                    If your contracts compiled and migrated successfully, below will show
-                    a stored value of 5 (by default).
-                </p>
-                <p>
-                    Try changing the value stored on <strong>line 42</strong> of App.js.
-                </p>
-                <div>The stored value is: {this.state.storageValue}</div>
+                {/* TODO: Implement navbar and a general welcome page */}
+                <h1>Hot Campaigns!</h1>
+                <div className="App-campaigns">
+                    <Row xs={1} md={5} className="g-4">
+                        {homeCampaigns.map((data, idx) => (
+                            <Col>
+                                <Card bg={"dark"}>
+                                    <Card.Img variant="top" src={data["image"]} />
+                                    <Card.Body>
+                                        <Card.Title className="Card-title">{data['title']}</Card.Title>
+                                        <Card.Text className="Card-adr">{data['adr']}</Card.Text>
+                                        <Card.Text className="Card-desc">{data['desc']}</Card.Text>
+                                        {/* TODO: Add progbar */}
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
             </div>
         );
     }
