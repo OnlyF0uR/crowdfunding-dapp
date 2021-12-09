7d0f43de218c4e9f650727f2a515d44d68af6a2d
BuildTools <jerskisnow@gmail.com>
2021-12-09 17:22:56 +0100

    Afternoon update

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.10;
+pragma solidity ^0.8.10;
+import './ReentrancyGuard.sol';
 
-contract CrowdFunding {
+contract CrowdFunding is ReentrancyGuard {
+
     address owner;
 
-    constructor() {
+    constructor() ReentrancyGuard() {
         owner = msg.sender;
     }
 
@@ -23,6 +25,12 @@ contract CrowdFunding {
 
     uint256 prevId;
 
+    function submit() public payable {
+        // TODO: Manufactur a somewhat consistent price taking into account future gas prices
+        (bool sent, ) = payable(owner).call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+    }
+
     function mint(address host, uint256 exp) public adminOnly {
         // Increment the id
         prevId++;
@@ -30,41 +38,31 @@ contract CrowdFunding {
         campaigns[prevId] = Campaign(host, 0, exp);
     }
 
-    function claim(uint256 id) public {
+    function claim(uint256 id) public nonReentrant {
         // Check for authority
-        require(
-            campaigns[id].host == msg.sender,
-            "You can only claim your own campaign."
-        );
+        require(campaigns[id].host == msg.sender, "You can only claim your own campaign.");
         // Check if campaign can be claimed
-        require(
-            campaigns[id].expires > block.timestamp,
-            "Campaign is still active."
-        );
+        require(campaigns[id].expires > block.timestamp, "Campaign is still active.");
         // Check if there is money to be claimed
         require(campaigns[id].balance > 0);
 
         uint256 bal = campaigns[id].balance;
 
         // NOTE: Just for safety since this really should not happen.
-        require(
-            address(this).balance >= bal,
-            "The contract does not have enough liquidity, please try again later."
-        );
+        require(address(this).balance >= bal, "The contract does not have enough liquidity, please try again later.");
 
         // Transfer the money to the campaign hoster/claimer
-        // msg.sender.transfer(withdrawalAmount);
-        payable(msg.sender).transfer(bal);
+        (bool sent, ) = payable(msg.sender).call{value: bal}("");
+        require(sent, "Failed to send Ether");
     }
 
     function donate(uint256 id) public payable {
-        require(
-            campaigns[id].expires <= block.timestamp,
-            "Campaign has been closed."
-        );
+        require(campaigns[id].host != msg.sender, "A host cannot donate to their own campaign.");
+        require(campaigns[id].expires <= block.timestamp, "Campaign has been closed.");
 
-        // Deposit the money into the smart contract
-        payable(address(this)).transfer(msg.value);
+        // Transfer the money to the campaign hoster/claimer
+        (bool sent, ) = payable(address(this)).call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
     }
 
     function get(uint256 id)
